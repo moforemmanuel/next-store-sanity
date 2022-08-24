62098f4b62bbb6359b355e2b3965cf084b5af537
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-24 16:04:12 +0100

    added Store context

---
+++ utils/Store.js
@@ -0,0 +1,41 @@
+import Cookies from 'js-cookie';
+
+import { createContext, useReducer } from 'react';
+
+export const Store = createContext();
+
+const initialState = {
+  cart: {
+    cartItems: Cookies.get('cartItems')
+      ? JSON.parse(Cookies.get('cartItems'))
+      : [],
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CART_ADD_ITEM': {
+      const newItem = action.payload;
+      const existingItem = state.cart.cartItems.find(
+        (item) => item._key === newItem._key
+      );
+
+      const cartItems = existingItem
+        ? state.cart.cartItems.map(
+            (item) => (item._key === existingItem._key ? newItem : item) //update (use new item with updated quantity) quantity if exist else use old item
+          )
+        : [...state.cart.cartItems, newItem]; // add new item to end of cart
+      Cookies.set('cartItems', JSON.stringify(cartItems)); // save cartItems to cookies
+      return { ...state, cart: { ...state.cart, cartItems } }; //update only cart Items
+    }
+
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
--- utils/sanityImageBuilder.js
+++ utils/sanityImageBuilder.js
@@ -1,6 +1,6 @@
 import ImageUrlBuilder from '@sanity/image-url';
 import client from './sanityClient';
 
-export default function urlForThumbnail(src) {
-  return ImageUrlBuilder(client).image(src).url();
+export default function urlForThumbnail(src, width) {
+  return ImageUrlBuilder(client).image(src).width(width).url();
 }
