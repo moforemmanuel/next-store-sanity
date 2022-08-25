f3a0398cb5d7d58a340478bba1851062a3fee594
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-25 17:30:27 +0100

    minor changes

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  SANITY_PROJECT_ID: 'mxes33vt',
+  NODE_ENV: 'production',
+  SANITY_API_VERSION: 'v2021-10-21',
+  SANITY_AUTH_TOKEN:
+    'skn56ofjoeOu7qXZl0eFYskS6ISt6NyGkao6WNHtDWiAsTva5hfZ7BYJi2vBOtgEWXwPHHqLwGZwqpDIDhMDYyXRsE4i6QDYNGEHukxCUIyWKKO8zvocWLme8hMvM8zLrPzFLdxOwiFQBVO6V7SQ7eSmlx1yjGEdOfsc1PiYDFrQOMsv0lsH',
+};
+
+export default config;
